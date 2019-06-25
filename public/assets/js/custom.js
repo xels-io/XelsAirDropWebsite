1b436e75f2fd5bef3783bfd537a9674c94297817
Md. Abu bakker siddique <bakker311042@gmail.com>
2019-06-25 09:38:40 +0600

    Scrool add

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,18 +1,30 @@
-$(document).ready(function()
+$(document).ready(function ()
 {
-    $(document).on("change, keyup , mouseup", "#eth_token_amount", function()
+    $(document).on("change, keyup , mouseup", "#eth_token_amount", function ()
     {
         var sels = parseFloat($("#eth_token_amount").val());
         var xels_token = sels;
 
         $("#xels_amount").val(xels_token);
     });
+    // Page scroll
+    $('.page-scroll').click(function () {
+        var target = $(this).attr('href');
+        $('html, body').stop().animate({
+            scrollTop: ($(target).offset().top - 80)
+        }, 1000);
+        return false;
+    });
+    // Menu Hide
+//    $('.navbar-nav>li>.page-scroll').on('click', function () {
+//        $('.navbar-collapse').collapse('hide');
+//    });
 });
 
 function setTooltip(message, $this = '') {
     $($this).tooltip('hide')
-        .attr('data-original-title', message)
-        .tooltip('show');
+            .attr('data-original-title', message)
+            .tooltip('show');
 }
 
 function copyText(text) {
@@ -24,12 +36,12 @@ function copyText(text) {
     return true;
 }
 
-  $(document).on('click','.btn-copy',function(){
-      var textTarget = $($(this).attr('data-copy'));
-      copyText(textTarget.text());
-      setTooltip('Copied', this);
-      var _this = this;
-      setTimeout(function(){
-          $(_this).tooltip('hide').removeAttr('data-original-title');
-      },1000)
-  })
+$(document).on('click', '.btn-copy', function () {
+    var textTarget = $($(this).attr('data-copy'));
+    copyText(textTarget.text());
+    setTooltip('Copied', this);
+    var _this = this;
+    setTimeout(function () {
+        $(_this).tooltip('hide').removeAttr('data-original-title');
+    }, 1000)
+})
